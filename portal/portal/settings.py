696b31ea83a14caa0b56fc53476876d2da8fe32c
k.kaplin <k.kaplin@profsoft.pro>
2020-11-28 02:25:13 +0400

    added leaflet library

--- portal/portal/settings.py
+++ portal/portal/settings.py
@@ -90,7 +90,4 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = 'static'
