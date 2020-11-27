12aacc790e0032019326123d788672d214b3ce79
paqstd <zebartcoc@gmail.com>
2020-11-27 20:25:45 +0300

    Add test-page for voice parsing

--- portal/api/views.py
+++ portal/api/views.py
@@ -8,18 +8,27 @@ from django.http import JsonResponse
 
 @method_decorator(csrf_exempt, name='dispatch')
 class VioceParser(View):
+    def get(self, request):
+        return render(request, 'api/voice.html')
+
     def post(self, request):
         import speech_recognition as sr
 
         # get voice from file
-        sample_audio = sr.AudioFile(request.FILES.get('voice'))
-        recognizer = sr.Recognizer()
+        try:
+            sample_audio = sr.AudioFile(request.FILES.get('voice'))
+            recognizer = sr.Recognizer()
 
-        # opem voice file
-        with sample_audio as audio_file:
-            recognizer.adjust_for_ambient_noise(audio_file)
-            audio_content = recognizer.record(audio_file)
+            # opem voice file
+            with sample_audio as audio_file:
+                recognizer.adjust_for_ambient_noise(audio_file)
+                audio_content = recognizer.record(audio_file)
 
-        return JsonResponse({
-            'text': recognizer.recognize_google(audio_content, language='ru-RU')
-        })
+            return JsonResponse({
+                'error': False,
+                'text': recognizer.recognize_google(audio_content, language='ru-RU')
+            })
+        except:
+            return JsonResponse({
+                'error': True
+            }) 
