a1e770017bf40b643ee8c2ab7443c5ffa10252e0
paqstd <zebartcoc@gmail.com>
2020-11-27 22:06:57 +0300

    Added kanban some views

--- portal/main/urls.py
+++ portal/main/urls.py
@@ -3,5 +3,11 @@ from .views import *
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
+
+    # kanban
     path('kanban/', Kanban.as_view(), name='kanban-index'),
+    path('kanban/create/', KanbanForm.as_view(), name='kanban-create'),
+    path('kanban/<int:id>/edit/', KanbanForm.as_view(), name='kanban-edit'),
+    path('kanban/<int:id>/view/', KanbanView.as_view(), name='kanban-view'),
+    path('kanban/<int:id>/delete/', KanbanView.as_view(), name='kanban-delete'),
 ]
--- portal/main/views.py
+++ portal/main/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 
 from .models import Task
+from .forms import TaskForm
 class Index(View):
     def get(self, request):
         return render(request, 'main/index.html')
@@ -12,4 +13,37 @@ class Kanban(View):
 
         return render(request, 'kanban/index.html', {
             'tasks': tasks
+        })
+
+class KanbanView(View):
+    def get(self, request, id):
+        task = Task.objects.get(id=id)
+
+        return render(request, 'kanban/view.html', {
+            'task': task
+        })
+
+    def post(self, request, id):
+        task = Task.objects.get(id=id)
+        task.delete()
+
+        return redirect('kanban-index')  
+class KanbanForm(View):
+    def get(self, request, id=None):
+        form = TaskForm() if not id else TaskForm(instance=Task.objects.get(id=id))
+
+        return render(request, 'kanban/create.html', {
+            'form': form
+        })
+
+    def post(self, request, id=None):
+        form = TaskForm(request.POST, instance=None if not id else Task.objects.get(id=id))
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('kanban-index') if not id else redirect('kanban-view', id)
+
+        return render(request, 'kanban/create.html', {
+            'form': form
         })
