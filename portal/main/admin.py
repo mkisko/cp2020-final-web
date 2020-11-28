0a59ee5f1b305eb4cd250f6f5ed91d71f227aafc
paqstd <zebartcoc@gmail.com>
2020-11-28 15:56:08 +0300

    Regulations & form

--- portal/main/admin.py
+++ portal/main/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Task)
+admin.site.register(Regulations)
 admin.site.register(Comment)
 
+admin.site.register(Role)
 admin.site.register(Profile)
+
 admin.site.register(Question)
 admin.site.register(Answer)
--- portal/portal/settings.py
+++ portal/portal/settings.py
@@ -82,7 +82,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Internationalization
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
